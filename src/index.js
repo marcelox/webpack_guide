115fae23274e4c2ca6255206c5e22cfb26a4029a
Marcelo Cordeiro <hi@marcelo.berlin>
2018-09-13 12:01:17 +0200

    Cleans up the project

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import _ from 'lodash';
-import './style.css';
-import Unsplash from './stefan-cosma-521680-unsplash.jpg';
 function component() {
   let element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  var myImg = new Image();
-  myImg.src = Unsplash;
-
-  element.appendChild(myImg);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,40 +6,4 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
-    module: {
-      rules: [
-          {
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
-          },
-          {
-             test: /\.(png|svg|jpg|gif)$/,
-             use: [
-                 {
-                 loader: 'file-loader',
-                     options: {
-                         outputPath: 'images/'
-                     }
-                 },
-                 {
-                 loader: 'image-webpack-loader',
-                 }
-             ]
-          },
-          {
-              test:/\.(woff|woff2|eot|ttf|otf)$/,
-              use: [
-                  {
-                  loader: 'file-loader',
-                      options: {
-                        outputPath: 'fonts/'
-                      }
-                  }
-              ]
-          }
-      ]
-    }
 };
